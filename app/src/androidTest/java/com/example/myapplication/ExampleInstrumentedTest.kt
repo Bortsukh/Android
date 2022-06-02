65d3aa0dcaf61635c69d5a8efde350e1a8262b23
Bortsukh <bortsukh.stanislav@yandex.ru>
2022-06-02 23:19:08 +0300

    add tests

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -27,7 +27,6 @@ import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
